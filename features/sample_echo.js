fd91c77b8619b5d2c44bca75254a77bd22ce6d9b
Domi Yan <yanyue1991@gmail.com>
2019-09-21 18:17:49 +0000

    remove echo all message

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -5,12 +5,7 @@
 
 module.exports = function(controller) {
 
-    controller.hears('sample','message,direct_message', async(bot, message) => {
-        await bot.reply(message, 'I heard a sample message.');
-    });
-
     controller.on('message,direct_message', async(bot, message) => {
-        await bot.reply(message, `Echo: ${ message.text }`);
+        console.log(message.text) 
     });
-
 }
